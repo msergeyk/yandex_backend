75fd9610d24a696e058c25643e77e23221a7457c
msergeyk <msergey1996@mail.ru>
2021-03-28 18:31:23 +0000

    added database

---
+++ configs/db.py
@@ -0,0 +1,17 @@
+import os
+from models.base import Base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+PG_DB = 'sweets'
+PG_USER = 'postgres'
+PG_PASSWORD = 'pgpass'
+PG_HOST = 'localhost'
+PG_PORT = '8010'
+
+PG_URL = f'''postgresql+psycopg2://{PG_USER}:{PG_PASSWORD
+             }@{PG_HOST}:{PG_PORT}/{PG_DB}'''
+
+engine = create_engine(PG_URL, echo=False)
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
